feeb3fec5bbd0d0c1665ef836ffdb3de9cf04949
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-29 03:06:16 +0700

    add delete and fix routing for view, update,delete using rails route helper

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,38 +3,53 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
     end
+
 #show =>GET
     def show
       @book = Book.find(params[:id])
     end
+
 #new =>POST
     def new
         @book = Book.new
     end
+
 #create =>POST
     def create
         book = Book.new(resource_params)
         book.save
-        render plain: 'Berhasil menyimpan data buku'
+        redirect_to books_path, notice: "Berhasil menambah data buku"
+      end
+
+      #edit =>GET
+    def edit
+        @book = Book.find(params[:id])
     end
 
-  #edit =>GET
-      def edit
-        @book = Book.find(params[:id])
-      end
+      #update => Patch/Put
+    def update
+      @book = Book.find(params[:id])
+      @book.update(resource_params)
+      render plain: 'Berhasil mengubah data buku'
+    end
+
+     #destroy => DELETE
+     def destroy
+      book = Book.find(params[:id])
+     if book.destroy
+      redirect_to books_path, notice: "Berhasil menghapus data buku"
+      else
+      redirect_to books_path, notice: "Gagal menghapus data buku"
+     end
+    end
 
     private
 
     def resource_params
-        params.require(:book).permit(:title, :page, :description)
+      params.require(:book).permit(:title, :page, :description)
     end
 
-#update => Patch/Put
-    def update
-    end
-#destroy => DELETE
-    def destroy
-      book = Book.destroy
-      render :index
-    end
+
+
+
 end
