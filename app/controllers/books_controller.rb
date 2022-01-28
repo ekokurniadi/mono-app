2467b1ccbcad12a50179a9ea43b2aee8afc0590d
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-28 23:51:53 +0700

    create books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+#index =>GET
+    def index
+    end
+#show =>GET
+    def show
+    end
+#new =>POST
+    def new
+    end
+#create =>POST
+    def create
+    end
+#edit =>GET
+    def edit
+    end
+#update => Patch/Put
+    def update
+    end
+#destroy => DELETE
+    def destroy
+    end
+end
