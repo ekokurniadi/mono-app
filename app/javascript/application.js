c8e87309cd87a230cb66e2adb253b3b0ca264a73
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-28 23:46:17 +0700

    add javascript on RoR

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#btn-home").addEventListener("click", function() {
+        alert("Hello World!");
+    });
+})
