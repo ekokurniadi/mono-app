841830567d2f5e6c344d8b871e9734acc7e6f31e
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-28 22:26:37 +0700

    create instance variable for display string on view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
     def index
+        # instance variable using @
         @langs = ['ruby','php','java','go','c++']
+        @title = 'Hallo this is title'
     end
 
     def hallo
